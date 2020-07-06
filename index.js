e1ed3ce3151a46f3930c59c31c23a670d51ae106
ppoliani <ppoliani@gmail.com>
2020-07-06 12:22:31 +0100

    update index

--- index.js
+++ index.js
@@ -1,17 +1,70 @@
 const {app, BrowserWindow} = require('electron')
 
+// Add React extension for development
+const {
+  default: installExtension, 
+  REACT_DEVELOPER_TOOLS
+} = require('electron-devtools-installer')
+
+// Keep a reference for dev mode
+let dev = false
+
+// Determine the mode (dev or production)
+if (process.defaultApp || /[\\/]electron-prebuilt[\\/]/.test(process.execPath) || /[\\/]electron[\\/]/.test(process.execPath)) {
+  dev = true
+}
+
+// Temporary fix for broken high-dpi scale factor on Windows (125% scaling)
+// info: https://github.com/electron/electron/issues/9691
+if (process.platform === 'win32') {
+  app.commandLine.appendSwitch('high-dpi-support', 'true')
+  app.commandLine.appendSwitch('force-device-scale-factor', '1')
+}
+
 function createWindow () {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 768,
+    show: false, // don't show until window is ready
     webPreferences: {
       nodeIntegration: true
     }
   })
 
+  // Determine the correct index.html file
+  // (created by webpack) to load in dev and production
+  let indexPath
+
+  if (dev && process.argv.indexOf('--noDevServer') === -1) {
+    indexPath = url.format({
+      protocol: 'http:',
+      host: 'localhost:8080',
+      pathname: 'index.html',
+      slashes: true
+    })
+  } 
+  else {
+    indexPath = url.format({
+      protocol: 'file:',
+      pathname: path.join(__dirname, 'dist', 'index.html'),
+      slashes: true
+    })
+  }
+
   // and load the index.html of the app.
-  win.loadFile('./src/view/index.html')
+  win.loadFile(indexPath);
+
+  // Don't show the app window until it is ready and loaded
+  win.once('ready-to-show', () => {
+    win.show()
+    // Open the DevTools automatically if developing
+    if (dev) {
+      installExtension(REACT_DEVELOPER_TOOLS)
+        .catch(err => console.log('Error loading React DevTools: ', err))
+        win.webContents.openDevTools()
+    }
+  })
 
   // Open the DevTools.
   win.webContents.openDevTools()
